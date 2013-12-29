bbad9ee1045a25e726827896588c0f4de0dd4389
Dani Dickstein <danidickstein@yahoo.com>
2013-12-28 20:06:49 -0430

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+EliminationCircle::Application.config.secret_key_base = 'b373131cba2db2be0111cebd3ff78d0c3dad6f4f508accf505cf7468fe5c9dd180021d1d3d4624115cedeb0f7c6224281b3b9adfb4ae1928a5e29ca744ace28b'
