664a539f134ffd9dffb7da62de4fc581c19a25a3
Dani Dickstein <danidickstein@yahoo.com>
2013-12-28 22:56:10 -0430

    pages controller created

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 EliminationCircle::Application.routes.draw do
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'pages#about'
+  match '/about' => "pages#about"
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
